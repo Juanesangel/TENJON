c333c1570afdb52ea64686d68d93b91efe473b92
Juanesangel <j.angelp@uniandes.edu.co>
2026-03-26 14:29:47 -0500

    Primer commit proyecto Unreal limpio

---
+++ Source/TENJON/TENJON.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TENJON.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TENJON, "TENJON" );
+
+DEFINE_LOG_CATEGORY(LogTENJON)
---
+++ Source/TENJON/TENJONGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TENJONGameMode.h"
+
+ATENJONGameMode::ATENJONGameMode()
+{
+	// stub
+}
